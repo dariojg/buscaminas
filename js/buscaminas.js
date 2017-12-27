c7bc33ba831d29f7391f042d5afd1e0ac9b727c8
Dario Grinberg <dariogrinberg@gmail.com>
2017-12-27 19:33:11 -0300

    show and mark cells

--- js/buscaminas.js
+++ js/buscaminas.js
@@ -18,8 +18,35 @@ function drawBoard(){
     for(var i=0; i < rows; i++){
         for( var j = 0; j < cols; j++){
             var div = document.createElement("div")
+            div.id = i + "" + j 
+            div.addEventListener("contextmenu", markCell, false)
+            div.addEventListener("click", showCell, true)
             board_id.appendChild(div)
         }
+    }
+}
+
+function markCell(ev){
+    ev.preventDefault();
+    var div_id = this.id
+    divObj = document.getElementById(div_id);
+    divObj.style.backgroundColor = "blue";
+    return false;
+}
+
+function showCell(ev){
+    var number_mines = this.id.split("");	
+	var div_id = this.id			
+	divObj = document.getElementById(div_id);
+
+	if(arrayMatrix[parseInt(number_mines[0],10)][parseInt(number_mines[1],10)] != -1){
+		document.getElementById(div_id).innerHTML = "<p style='margin-top:5px;'>" + arrayMatrix[parseInt(number_mines[0],10)][parseInt(number_mines[1],10)] + "</p>";
+		divObj.style.backgroundColor = "white";
+    }
+    if(arrayMatrix[parseInt(number_mines[0],10)][parseInt(number_mines[1],10)] == -1){
+        divObj.style.backgroundColor = "red";
+        document.getElementById("boom").innerHTML = "<h1>BOOOM!</h1>";
+        document.getElementById("restart").style.display="block";
     }
 }
 
